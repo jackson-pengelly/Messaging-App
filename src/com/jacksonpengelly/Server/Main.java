69ec56b7717f26d65e02673c81ebd9afec47f191
j.pengelly <jfpngly@gmail.com>
2026-08-26 20:46:39 -0400

    Added IP display for host.
    + Gets the host of the servers IP and displays it so they can send to people to join the room
    
    + Added a dev mode that runs on localhost.

--- src/com/jacksonpengelly/Server/Main.java
+++ src/com/jacksonpengelly/Server/Main.java
@@ -3,8 +3,9 @@ package com.jacksonpengelly.Server;
 import com.jacksonpengelly.SharedConfig.ServerInformation;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -12,12 +13,14 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import static com.jacksonpengelly.Server.Config.Commands.*;
 
 public class Main {
+    private static final boolean DEVELOPMENT_MODE = true;
+
     public static final List<ClientHandler> activeClients = new CopyOnWriteArrayList<>();
 
     private static void start() {
         try (ServerSocket serverSocket = new ServerSocket(ServerInformation.GENERAL_PORT);
              Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Server started successfully; listening on general port: " + ServerInformation.GENERAL_PORT);
+            System.out.println("Server started successfully; IP: " + getServerIP() + "; listening on port: " + ServerInformation.GENERAL_PORT);
 
             // main thread to listen for messages sent on server
             Thread listenerThread = new Thread(() -> {
@@ -59,6 +62,29 @@ public class Main {
         } catch (IOException e) {
             System.err.println("Server Error: " + e.getMessage());
         }
+    }
+
+    private static String getServerIP() {
+        if (DEVELOPMENT_MODE) return "127.0.0.1";
+
+        try {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            for (NetworkInterface netInterface : Collections.list(interfaces)) {
+                if (netInterface.isLoopback() || !netInterface.isUp() || netInterface.isVirtual()) {
+                    continue;
+                }
+
+                Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
+                for (InetAddress address : Collections.list(addresses)) {
+                    if (!address.isLoopbackAddress() && address.getHostAddress().indexOf(':') == -1) {
+                        return address.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            System.err.println("Unknown IP Error: " + e.getMessage());
+        }
+        return "127.0.0.1 (Localhost)";
     }
 
     private static void help() {
