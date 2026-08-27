16ae1e13eb2f218d615e9e5af60ab53bf841e58d
j.pengelly <jfpngly@gmail.com>
2026-08-27 18:18:50 -0400

    PostgreSQL Database
    + Created the baseplate files for the PostgreSQL database.
    
    + Added PostgreSQL driver to the Maven dependencies.

---
+++ sql/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS servers (
+    id SERIAL PRIMARY KEY,
+    server_name VARCHAR(100) NOT NULL,
+    is_public BOOLEAN NOT NULL DEFAULT TRUE,
+    password_hash CHAR(60) NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
